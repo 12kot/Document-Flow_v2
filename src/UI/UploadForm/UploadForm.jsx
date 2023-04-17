be29a8e72ff1b70408e571d38b5728a5c3ee2592
Nikita Yakoltsevich <Art.artem.nik@gmail.com>
2023-04-17 11:52:57 +0300

    added drag and drop

--- src/UI/UploadForm/UploadForm.jsx
+++ src/UI/UploadForm/UploadForm.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./UploadForm.module.css";
 import Loader from "../Loader/Loader";
 
 const UploadForm = (props) => {
+  const [drag, setDrag] = useState(false);
+
+  const dragStartHandler = (e) => {
+    e.preventDefault();
+    setDrag(true);
+  }
+
+  const dragLeaveHandler = (e) => { 
+    e.preventDefault();
+    setDrag(false);
+  }
+
+  const onDropHandler = (e) => { 
+    e.preventDefault();
+    uploadFile(...e.dataTransfer.files);
+
+    setDrag(false);
+  }
+  
   const uploadFile = (file) => {
+    debugger
     props.uploadFile(file);
   };
 
@@ -12,12 +32,16 @@ const UploadForm = (props) => {
       <form className={styles.form}>
         <span className={styles.form_title}>Загрузите свой файл</span>
         <p className={styles.form_paragraph}>Загружайте различные типы файлов</p>
-        <label htmlFor="file-input" className={styles.drop_container}>
+        <label htmlFor="file-input" className={styles.drop_container}
+          onDragStart={(e) => dragStartHandler(e) }
+          onDragLeave={(e) => dragLeaveHandler(e) }
+          onDragOver={(e) => dragStartHandler(e) }
+          onDrop={(e) => onDropHandler(e)}>
           {props.isUploadLoading ? (
             <Loader />
           ) : (
             <>
-              <span className={styles.drop_title}>Перетащи файл сюда</span>
+              <span className={styles.drop_title}> {drag ? "Отпусти файл, чтобы загрузить" : "Перетащи файл сюда"}</span>
               или
               <input
                 type="file"
